84f5d6b49d7e79452f820dc8d016142cefae3a91
Primero <alonsomunoz263@gmail.com>
2023-03-20 16:30:12 -0500

    Tercero

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function finalizar(){
+    alert("Hemos concluido la tarea");
+}
+
+function finalizaremos(){
+    alert("Ahora si, hemos finalizado la tarea");
+}
+
+const finalizamos = document.getElementById("finalizar");
+finalizamos.addEventListener("click", finalizaremos);
